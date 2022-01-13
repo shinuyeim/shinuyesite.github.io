9b4d85117e81a9e12e198f96f18ed25fd544c68e
shinuyeim <shinuyemail@gmail.com>
2022-01-13 20:59:35 +0800

    site rename

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from 'vuepress'
 import type { DefaultThemeOptions } from 'vuepress'
 
 export default defineUserConfig<DefaultThemeOptions>({
-  base: "/shinuyesite.github.io/",
+  base: "/space/",
   title: "午后南杂",
   description: 'Enjoy when you can, and endure when you must.',
   dest: 'public',
