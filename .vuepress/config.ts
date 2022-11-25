0ac0d04ee8df9326a4dc9a866051d05bce43db08
shinuyeim <shinuyemail@gmail.com>
2022-11-25 22:39:53 +0800

    修改icon

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -7,7 +7,7 @@ export default defineUserConfig<DefaultThemeOptions>({
   description: 'Enjoy when you can, and endure when you must.',
   dest: 'public',
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['link', { rel: 'icon', href: '/favicon1.ico' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
   ],
   theme: 'reco',
